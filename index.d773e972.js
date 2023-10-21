02c38554fee28e8b8b23703570b645e3774445ba
Matvii-Alkhimov <112165072+Matvii-Alkhimov@users.noreply.github.com>
2023-10-21 08:45:39 +0000

    Deploying to gh-pages from @ Matvii-Alkhimov/Team_Project@728a2c1e61368c87b2dd4da2f931d9e539156d11 🚀

---
+++ index.d773e972.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("."),n=document.querySelector("."),t=document.querySelector("body");function c(e){e.code}n.addEventListener("click",(function(){t.classList.add("show-modal"),window.addEventListener("keydown",c)})),e.addEventListener("click",(function(e){}))}();
+//# sourceMappingURL=index.d773e972.js.map
