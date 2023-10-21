365ded3e5eae636499d77d897b8c864dd983fe65
Matvii-Alkhimov <112165072+Matvii-Alkhimov@users.noreply.github.com>
2023-10-21 09:23:02 +0000

    Deploying to gh-pages from @ Matvii-Alkhimov/Team_Project@515aa84f5b7817e0f35fef0d996b0e8c907df0bc 🚀

--- index.238034b0.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("."),d=document.querySelector("."),n=document.querySelector("body");function t(e){e.code}d.addEventListener("click",(()=>{n.classList.add("show-modal"),window.addEventListener("keydown",t)})),e.addEventListener("click",(function(e){}));
-//# sourceMappingURL=index.238034b0.js.map
