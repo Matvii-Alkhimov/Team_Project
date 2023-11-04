b08be24b9cd5905c16c9d139cd92f4f095be7354
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2023-11-04 11:19:44 +0200

    add info

--- src/js/closing-modal-esc.js
+++ src/js/closing-modal-esc.js
@@ -1,59 +1,59 @@
-const backdrop = document.querySelector(".backdrop")
-const openModale = document.querySelector('[data-action="open-modal"]')
-const closeModale = document.querySelector('[data-action="close-modal"]')
-const modale = document.querySelector("[data-modal]")
-const body = document.querySelector("body")
+// const backdrop = document.querySelector(".backdrop")
+// const openModale = document.querySelector('[data-action="open-modal"]')
+// const closeModale = document.querySelector('[data-action="close-modal"]')
+// const modale = document.querySelector("[data-modal]")
+// const body = document.querySelector("body")
 
-<<<<<<< Updated upstream
-openModale.addEventListener("click", (event) => {
-    console.log(event.target.nodeName)
-   if (event.target.nodeName === "IMG") {
-      backdrop.classList.remove("is-hidden")
-      window.addEventListener("keydown", onModalEscClose)
-   }
-})
+// // <<<<<<< Updated upstream
+// openModale.addEventListener("click", (event) => {
+//     console.log(event.target.nodeName)
+//    if (event.target.nodeName === "IMG") {
+//       backdrop.classList.remove("is-hidden")
+//       window.addEventListener("keydown", onModalEscClose)
+//    }
+// })
 
-closeModale.addEventListener("click", () => {
-   backdrop.classList.add("is-hidden")
-=======
-openModale.addEventListener("click", () => {
-  body.classList.add("show-modal")
-  window.addEventListener("keydown", onModalEscClose)
->>>>>>> Stashed changes
-})
+// closeModale.addEventListener("click", () => {
+//    backdrop.classList.add("is-hidden")
+// // =======
+// openModale.addEventListener("click", () => {
+//   body.classList.add("show-modal")
+//   window.addEventListener("keydown", onModalEscClose)
+// >>>>>>> Stashed changes
+// })
 
-closeModale.addEventListener("click", onBtnClose)
+// closeModale.addEventListener("click", onBtnClose)
 
-function onBtnClose() {
-<<<<<<< Updated upstream
-    backdrop.classList.add("is-hidden")
-    window.removeEventListener("keydown", onModalEscClose);
-=======
-    body.classList.remove("show-modal")
-    window.removeEventListener("keydown" , onModalEscClose);
->>>>>>> Stashed changes
-}
+// function onBtnClose() {
+// <<<<<<< Updated upstream
+//     backdrop.classList.add("is-hidden")
+//     window.removeEventListener("keydown", onModalEscClose);
+// =======
+//     body.classList.remove("show-modal")
+//     window.removeEventListener("keydown" , onModalEscClose);
+// >>>>>>> Stashed changes
+// }
 
-backdrop.addEventListener("click", onBackdropClick)
+// backdrop.addEventListener("click", onBackdropClick)
 
-function onBackdropClick(event) {
-    if (event.currentTarget === event.target) {
-        onBtnClose()
-    }
-}
+// function onBackdropClick(event) {
+//     if (event.currentTarget === event.target) {
+//         onBtnClose()
+//     }
+// }
 
-function onModalEscClose(evt){
-    if (evt.code === "Escape") {
-        onBtnClose() 
-    }
-}
+// function onModalEscClose(evt){
+//     if (evt.code === "Escape") {
+//         onBtnClose() 
+//     }
+// }
 
-// по кнопці
+// // по кнопці
 
-openModale.addEventListener("click", toggleModal)
-closeModale.addEventListener("click", toggleModal)
+// openModale.addEventListener("click", toggleModal)
+// closeModale.addEventListener("click", toggleModal)
 
-function toggleModal(){
-    document.body.classList.toggle("modal-open")
-    modale.classList.toggle("is-hidden")
-}
+// function toggleModal(){
+//     document.body.classList.toggle("modal-open")
+//     modale.classList.toggle("is-hidden")
+// }
--- src/js/main-section.js
+++ src/js/main-section.js
@@ -2,13 +2,19 @@
 import cardsTemplate from "../cards-render.handlebars"
 const cardsUl = document.querySelector('.cards-list')
 
-fetch("https://app.ticketmaster.com/discovery/v2/events.json?apikey=wfAi1gocagJe9lBfGlvRi4NoldweukkN")
-.then(res=> res.json())
+const fetchEvents = async() => {
+  const response= await fetch("https://app.ticketmaster.com/discovery/v2/events.json?apikey=wfAi1gocagJe9lBfGlvRi4NoldweukkN")
+  const events = await response.json()
+  return events;
+}
+
+fetchEvents()
 .then(data=>data._embedded.events)
 .then(events => {
     const markup = cardsTemplate(events)
     cardsUl.innerHTML = markup
 })
+.catch(error => console.log(error))
 
 
 
