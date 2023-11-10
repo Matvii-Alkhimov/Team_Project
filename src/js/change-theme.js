c061dfb8c06966d35207e4a2dd0545fdb37e8904
Maria89698 <maria9parhomova@gmail.com>
2023-11-10 17:38:00 +0100

    додає зображення

--- src/js/change-theme.js
+++ src/js/change-theme.js
@@ -1,3 +1,6 @@
+import darkMode from "../img/change-theme/dark_mode.svg"
+import lightMode from "../img/change-theme/light_mode.svg"
+
 const themeToggle = document.getElementById('theme-toggle');
 const icon = document.querySelector('.icon');
 
@@ -5,9 +8,9 @@ themeToggle.addEventListener('change', onToggleChange);
 
 function onToggleChange() {
     if (themeToggle.checked) {
-        icon.style.backgroundImage = 'url(/src/change-theme/dark_mode.svg)';
+        icon.style.backgroundImage = `url(${darkMode})`;
     } else {
-        icon.style.backgroundImage = 'url(/src/change-theme/light_mode.svg)';
+        icon.style.backgroundImage = `url(${lightMode})`;
     }
 }
 
