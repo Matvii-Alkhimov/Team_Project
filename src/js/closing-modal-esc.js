436a11b72fb0659a07120a8f8c07361477395907
Vlnduwfsfrekves <112093090+Vlnduwfsfrekves@users.noreply.github.com>
2023-11-12 18:54:21 +0200

    -mo

--- src/js/closing-modal-esc.js
+++ src/js/closing-modal-esc.js
@@ -60,8 +60,23 @@ const body = document.querySelector("body")
 openModale.addEventListener("click", (event) => {
     console.log(event.target.nodeName)
    if (event.target.nodeName === "IMG") {
-      backdrop.classList.remove("is-hidden")
-      window.addEventListener("keydown", onModalEscClose)
+    let id = event.target.id
+    const img = document.querySelector('.img1')
+    const img1 = document.querySelector('.ima')
+    const li = event.target.closest('li') 
+    const nameElement = li.querySelector('.name') 
+    const name = nameElement.textContent 
+    document.querySelector('.p-info').textContent = `${name} is the largest music festival` 
+    document.querySelector('.p-info:nth-child(4)').textContent = `${liElement.querySelector('.date').textContent}`
+    document.querySelector('.p-info:nth-child(7)').textContent = `${liElement.querySelector('.place').textContent}` 
+    console.log(id);
+    img1.style = "border-radius: 50%;width:132px; height:132px"
+    fetch(`https://pixabay.com/api/?key=39207344-802fb38289e47f3cf2d375300&id=${id}`) 
+    .then(resp => resp.json()) 
+    .then(re => img.scr = re)
+    .then(r => img1.scr = r) 
+    backdrop.classList.remove("is-hidden")
+    window.addEventListener("keydown", onModalEscClose)
    }
 })
 
