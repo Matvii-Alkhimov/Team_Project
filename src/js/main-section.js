5a680a7a69e469f7276b27f674919eeeb6ea6370
Matvii-Alkhimov <112165072+Matvii-Alkhimov@users.noreply.github.com>
2023-11-18 10:36:09 +0200

    Додає коміт

--- src/js/main-section.js
+++ src/js/main-section.js
@@ -2,6 +2,7 @@
 import cardsTemplate from "../cards-render.handlebars"
 const cardsUl = document.querySelector('.cards-list')
 const likedButton = document.querySelector(".liked-button")
+console.log(likedButton)
 
 const fetchEvents = async() => {
   const response= await fetch("https://app.ticketmaster.com/discovery/v2/events.json?apikey=wfAi1gocagJe9lBfGlvRi4NoldweukkN")
@@ -18,10 +19,10 @@ fetchEvents()
 .catch(error => console.log(error))
 
 
-likedButton.addEventListener("click", changeTheme)
+// likedButton.addEventListener("click", changeTheme)
 
 function changeTheme(){
-  likedButton.classList.add(liked)
+  likedButton.classList.add("liked")
 }
 
 
