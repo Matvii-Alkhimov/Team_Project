7a4fb06f937717a800abef7d1f1d5fe6cc31e7c7
Diana1246 <109847803+Diana1246@users.noreply.github.com>
2023-11-18 12:13:02 +0200

    dropdown

--- src/js/country-list.js
+++ src/js/country-list.js
@@ -1,22 +1,7 @@
 import cardsTemplate from '../cards-render.handlebars';
-
- const select = document.getElementById('countryDropdown');
- const container = document.getElementById('results');
-
- const API_KEY = 'qn9etkDNhtdkZcjBBZRgAISaFvIyDaIP';
-
-
- select.addEventListener('change' , async (event) => {
-  const country = event.target.value;
-
-  const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?countryCode=${country}&apikey=${API_KEY}`);  
-
-    const data = await response.json();
-    const events = data._embedded.events;
-import cardsTemplate from "../cards-render.handlebars"
-
+ 
 const select = document.getElementById('countryDropdown');
-const container = document.getElementById('results');
+const container = document.querySelector('.cards-list');
 
 const API_KEY = 'qn9etkDNhtdkZcjBBZRgAISaFvIyDaIP';
 
@@ -27,22 +12,17 @@ select.addEventListener("change", async (event) => {
     const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?countryCode=${country}&apikey=${API_KEY}`);
    
     const data = await response.json();
+ if (!data._embedded){
+  return
+ }
     const events = data._embedded.events;
 
-    const filteredEvents = events.filter(event => event.countryCode === country);
-  
-    const cardsHtml = cardsTemplate(filteredEvents); 
-
+   
+    const cardsHtml = cardsTemplate(events); 
+ 
 
  container.innerHTML = cardsHtml;
 
 });
 
-    const filteredEvents = events.filter(event => event.countryCode === country);
-
-    const cardsHtml = cardsTemplate(filteredEvents); 
-
-
-    container.innerHTML = cardsHtml;
-
- })
+  
