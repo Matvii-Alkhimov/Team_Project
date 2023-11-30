157b68092ebe3cebaf97931970b2c85537481d6a
Vlnduwfsfrekves <112093090+Vlnduwfsfrekves@users.noreply.github.com>
2023-11-30 21:18:39 +0200

    -modal

--- src/js/closing-modal-esc.js
+++ src/js/closing-modal-esc.js
@@ -60,22 +60,23 @@ const body = document.querySelector("body")
 openModale.addEventListener("click", (event) => {
     console.log(event.target.nodeName)
    if (event.target.nodeName === "IMG") {
-    // let id = event.target.id
-    const img = document.querySelector('.img1')
-    // console.log(img);
-    const img1 = document.querySelector('.img2')
+    let id = event.target.id
+    const img = document.querySelector('.img2')
+    const img1 = document.querySelector('.img1')
     const li = event.target.closest('li') 
     const nameElement = li.querySelector('.name') 
-    // const name = nameElement.textContent 
-    // document.querySelector('.p-info').textContent = `${name} is the largest music festival` 
-    // document.querySelector('.p-info:nth-child(4)').textContent = `${li.querySelector('.date').textContent}`
-    // document.querySelector('.p-info:nth-child(7)').textContent = `${li.querySelector('.place').textContent}`
+    const name = nameElement.textContent 
+    document.querySelector('.p-info').textContent = `${name} is the largest music festival` 
+    document.querySelector('.p-info:nth-child(4)').textContent = `${li.querySelector('.date').textContent}`
+    document.querySelector('.p-info:nth-child(7)').textContent = `${li.querySelector('.place').textContent}`
     console.log(img,img1,nameElement,document.querySelector('.p-info'))
-    // console.log(id);
-    // img1.style = "border-radius: 50%;width:132px; height:132px"
-    // fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=wfAi1gocagJe9lBfGlvRi4NoldweukkN&id=${id}`) 
-    // .then(resp => resp.json()) 
-    // .then(re => img.src = re['_embedded']['events'][0]['images'][0]['url'])
+    img1.style = "border-radius: 50%;width:132px; height:132px"
+    img.style = "width:700px;height:400px"
+    fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=wfAi1gocagJe9lBfGlvRi4NoldweukkN&id=${id}`) 
+    .then(resp => resp.json())
+    .then(re => img.src = re['_embedded']['events'][0]['images'][0]['url'])
+    .then(r => img1.src = r)
+    console.log(backdrop);
     backdrop.classList.remove("is-hidden")
     window.addEventListener("keydown", onModalEscClose)
    }
