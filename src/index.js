afc94eea4f936b63e47afffbe529f8bb78fb4e38
Maria89698 <maria9parhomova@gmail.com>
2023-12-01 16:57:59 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import './js/header';
 import './js/main-section';
 import './js/eventLikes'
 import './js/modal';
+import './js/change-theme';
 import './js/pagination';
 import './js/closing-modal-esc';
-import './js/change-theme';
 import './js/country-list';
 import './js/footer';
